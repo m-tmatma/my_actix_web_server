631775a5d39cc3892144d230fbdc03622964cc7f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-09-23 20:54:35 +0900

    add json handler

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use serde_json::json;
 
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello, Actix-web!")
+}
+
+async fn json_handler() -> impl Responder {
+    HttpResponse::Ok().json(json!({
+        "name": "John Doe",
+        "age": 43,
+        "is_student": false
+    }))
 }
 
 #[tokio::main]
@@ -9,6 +18,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(hello))
+            .route("/json", web::get().to(json_handler))
     })
     .bind("127.0.0.1:8080")?
     .run()
